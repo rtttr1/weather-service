02593009a7cb5260282183a3ab7a5a017fa0973e
kimkyuhong <bobo3766@naver.com>
2025-02-01 03:17:48 +0900

    init: svgr 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), vanillaExtractPlugin()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    vanillaExtractPlugin(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
 });
