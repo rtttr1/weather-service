f858d3dc4f04f20e343a6557a74d887b7e3a2e16
kimkyuhong <bobo3766@naver.com>
2025-02-05 17:15:41 +0900

    feat: 날씨 하이라이트 정보 카드 구현

---
+++ src/components/WeatherHighlightsCard/index.css.ts
@@ -0,0 +1,12 @@
+import { style } from '@vanilla-extract/css';
+
+export const containerStyle = style({
+  display: 'flex',
+  flexDirection: 'column',
+
+  width: '23rem',
+  height: '22rem',
+  padding: '2.5rem',
+
+  backgroundColor: '#ffffff',
+});
