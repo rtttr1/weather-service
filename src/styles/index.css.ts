f9377ac5f4f75d6de2c9ae946777b5756d912cce
kimkyuhong <bobo3766@naver.com>
2025-02-08 02:07:28 +0900

    style: 전체 컨테이너 속성 지정

--- src/styles/index.css.ts
+++ src/styles/index.css.ts
@@ -1,2 +1,18 @@
 import '@/styles/global.css';
 import '@/styles/reset.css';
+import { style } from '@vanilla-extract/css';
+
+export const containerStyle = style({
+  display: 'flex',
+  gap: '10rem',
+
+  width: '100%',
+  height: '100%',
+  backgroundColor: '#F2F0FE',
+
+  '@media': {
+    'screen and (max-width: 480px)': {
+      flexDirection: 'column',
+    },
+  },
+});
